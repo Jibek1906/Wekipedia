bd599c34c5001088dcb35d2fc07e4c310322bac3
OnePuchPotato <73746820+Jibek1906@users.noreply.github.com>
2023-12-01 22:54:54 +0600

    Adding Google font

--- Dogkipedia/lib/pages/intro_page.dart
+++ Dogkipedia/lib/pages/intro_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
-  const IntroPage({super.key});
+  const IntroPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 157, 226, 192),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+        children: [
+          const SizedBox(height: 30,),
+          //site name
+          Text(
+            "Dogkipedia",
+              style: GoogleFonts.archivoBlack(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+
+          const SizedBox(height: 28,),
+          
+          //icon
+          
+          //title
+          
+          //subtitle
+          
+          //start button
+         ],
+        ),
+       ),
+     );
+   }
 }
