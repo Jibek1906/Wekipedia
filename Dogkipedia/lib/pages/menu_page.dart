95818d6ed1cb9a086cdb5e54b840374d697d7db3
adel mukashova <100151471+adelm134@users.noreply.github.com>
2023-12-03 02:33:42 +0600

    Update menu_page.dart

--- Dogkipedia/lib/pages/menu_page.dart
+++ Dogkipedia/lib/pages/menu_page.dart
@@ -1,8 +1,12 @@
+import 'package:dogkipedia/components/button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MenuPage extends StatefulWidget {
+  const MenuPage({super.key});
+
   @override
-  _MenuPageState createState() => _MenuPageState();
+  State<MenuPage> createState() => _MenuPageState();
 }
 
 class _MenuPageState extends State<MenuPage> {
@@ -10,12 +14,44 @@ class _MenuPageState extends State<MenuPage> {
   Widget build(BuildContext context) {
     // Implement your MenuPage widget here
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Menu Page'),
-      ),
-      body: Center(
-        child: Text('This is the Menu Page'),
-      ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(),
+      body: Column(children: [
+        Container(  
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 84, 171, 126),
+            borderRadius: BorderRadius.circular(20)
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          padding: const EdgeInsets.all(25),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Text(
+                  'For Dog lovers',
+                    style: GoogleFonts.archivoBlack(
+                        fontSize: 20,
+                        color: Colors.white
+                    ),
+                  ),
+                    const SizedBox(height: 20),
+                    MyButton(text: 'Sign in', onTap: (){})
+                ]
+              ),
+              
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Image.asset('lib/images/smile.png'),
+                ),
+              ),
+            ]
+          ),
+        )
+      ]),
     );
   }
 }
