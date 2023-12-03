ea5b9ee6f7bbca91757afb6b784bc3f7e252b92d
OnePuchPotato <73746820+Jibek1906@users.noreply.github.com>
2023-12-03 21:15:45 +0600

    Username TextField

--- Dogkipedia/lib/pages/login_page.dart
+++ Dogkipedia/lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key});
@@ -19,10 +20,36 @@ class LoginPage extends StatelessWidget {
                 size: 100,
               ),
 
+              const SizedBox(height: 50),
               // welcome
+              Text(
+                "Welcome to Dogkipedia",
+                style: GoogleFonts.archivoBlack(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+
+              const SizedBox(height: 25),
 
               // username
-
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                 decoration: InputDecoration(
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Color.fromARGB(255, 189, 229, 201),)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color.fromARGB(
+                        255, 117, 155, 118))
+                    ),
+                   fillColor: Color.fromARGB(255, 189, 229, 201),
+                   
+                   filled: true,
+                  ),
+                ),
+              ),
               // password
 
               // forgot password?
