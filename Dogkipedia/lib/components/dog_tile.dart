fc25216350e95da09fc1c9b27a17dc2e78c350e1
adel mukashova <100151471+adelm134@users.noreply.github.com>
2023-12-08 22:39:14 +0600

    Update dog_tile.dart

--- Dogkipedia/lib/components/dog_tile.dart
+++ Dogkipedia/lib/components/dog_tile.dart
@@ -4,47 +4,54 @@ import '../models/dog.dart';
 
 class DogTile extends StatelessWidget {
   final Dog dog;
+  final void Function()? onTap;
+
   const DogTile({
     super.key,
-    required this.dog
+    required this.dog,
+    required this.onTap,
 });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 157, 226, 192),
-        borderRadius: BorderRadius.circular(20),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 157, 226, 192),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        margin: const EdgeInsets.all(15),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              dog.imagePath,
+              height: 120,
+            ),
+      
+            Text(
+              dog.name,
+              style: GoogleFonts.archivoBlack(fontSize: 15),
+            ),
+      
+            SizedBox(
+              width: 160,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(dog.type, style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ))
+                ],
+              )
+            ),
+          ],
+        )
       ),
-      margin: const EdgeInsets.all(15),
-      padding: const EdgeInsets.all(5),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            dog.imagePath,
-            height: 120,
-          ),
-
-          Text(
-            dog.name,
-            style: GoogleFonts.archivoBlack(fontSize: 15),
-          ),
-
-          SizedBox(
-            width: 160,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(dog.type, style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black
-                ))
-              ],
-            )
-          ),
-        ],
-      )
     );
   }
 }
