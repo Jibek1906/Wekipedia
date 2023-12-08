502cbae303358af14bddbde3ec0740d7a82f9826
adel mukashova <100151471+adelm134@users.noreply.github.com>
2023-12-08 22:40:08 +0600

    Update dog.dart

--- Dogkipedia/lib/models/dog.dart
+++ Dogkipedia/lib/models/dog.dart
@@ -2,14 +2,17 @@ class Dog {
   String name;
   String type;
   String imagePath;
+  String description;
 
   Dog({
     required this.name,
     required this.type,
     required this.imagePath,
+    required this.description,
   });
 
   String get _name => name;
   String get _type => type;
   String get _imagePath => imagePath;
+  String get _description => description;
 }
